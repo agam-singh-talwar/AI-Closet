967530c74544047636f0c7d8d1cee9895c57ca51
Maria <79389256+marythedev@users.noreply.github.com>
2024-03-22 12:30:48 -0400

    basic setup (db doent connect)

--- index.js
+++ index.js
@@ -1,11 +1,78 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+const express = require("express");
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to AI Clauset!');
+//set up application
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+//set up middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//connect to db & start the app
+const uri = process.env.MONGODB_URI;
+
+const connectToDB = () => {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(uri)
+            .then(() => {
+                console.log("Database connection established.");
+                resolve();
+            })
+            .catch((error) => {
+                reject("Can't connect to the database. " + error);
+            });
+    });
+};
+
+connectToDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Application is listening on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("App failed to start. " + error);
+    });
+
+
+//app routes
+app.get("/", (request, response) => {
+    response.status(200).render("index", { title: "Not Found" });
 });
 
-app.listen(port, () => {
-  console.log(`App is listening at port ${port}`);
+app.get("/login", (request, response) => {
+    // render login page
+});
+
+app.post("/login", (request, response) => {
+    // login
+});
+
+app.get("/signup", (request, response) => {
+    // render signup page
+});
+
+app.post("/signup", (request, response) => {
+    // signup
+});
+
+// the next couple of routes require middleware to not allow outside access
+
+// app.get("/account", checkAuthorization, (request, response) => {
+//     // render account page, etc...
+// });
+
+// app.post("/account", checkAuthorization, (request, response) => {
+//     // update account
+// });
+
+app.get("/logout", (request, response) => {
+    // reset session and redirect to the main home page
+});
+app.get("/*", (request, response) => {
+    // render page 404
 });
